1667fcd0d327171177c9b1a5a42c037124657083
Lucas Rech <lucasrech00@gmail.com>
2025-10-17 20:42:51 -0300

    corrigido variabel tablename

--- server/src/model/__init__.py
+++ server/src/model/__init__.py
@@ -7,11 +7,18 @@ Base = declarative_base()
 
 class Produto(Base):
     
-    ___tablename__ = "produto"
+    __tablename__ = "produto"
     
     id = Column(Integer, primary_key=True, autoincrement="auto")
     gtin = Column(String, unique=True, nullable=False)
     nome = Column(String(255), unique=True, nullable=False)
     descricao = Column(String(500), unique=False, nullable=True)
     
-    
+
+
+
+
+
+
+#Deve ficar no final do arquivo
+Base.metadata.create_all(engine)
