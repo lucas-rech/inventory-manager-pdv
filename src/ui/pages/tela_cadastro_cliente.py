45bc909256bf93bb1cabb1b8fcdf42218c454925
Bruno Bortolon <bbortolondasilva@gmail.com>
2025-10-20 21:18:09 -0300

    Adicionar seleção CPF/CNPJ

--- src/ui/pages/tela_cadastro_cliente.py
+++ src/ui/pages/tela_cadastro_cliente.py
@@ -3,10 +3,38 @@ from ui.components.botoes.botao_adicionar import criar_botao_adicionar
 from ui.components.botoes.botao_cancelar import criar_botao_cancelar
 
 def cadastrar_clientes(page, clientes):
+    # Função que exibirá cada campo (CPF/CNPJ) com sua formatação própria.
+    def selecionar_cpf_cnpj(e):
+        if cpf_cnpj.value == "cpf": # Teste se o valor selecionado é "cpf"
+            print("CPF")
+
+        elif cpf_cnpj.value == "cnpj": # Teste se o valor selecionado é "cpnj"
+            print("CNPJ")
+
+
     # Campos do formulário para o cadastro de um cliente:
     nome = ft.TextField(label="Nome do cliente", bgcolor=ft.Colors.WHITE, width=610)
 
     numero = ft.TextField(label="Número de telefone do cliente", hint_text="Ex: (XX) XXXX-XXXX", bgcolor=ft.Colors.WHITE, width=610)
+
+    # O campo de cadastro do cpf ou cnpj do cliente será diferente para cada tipo, mudando a formatação do campo de acordo com o dado que será diigtado.
+    cpf_cnpj = ft.RadioGroup( # Grupo de seleção
+        content=ft.Column( # O conteúdo é composto por uma coluna com as opções
+            controls=[ # Os controles serão as opções
+                ft.Radio(label="CPF", value="cpf"), # Opção cpf, altera o value da variável "cpf_cnpj" para cpf.
+
+                ft.Radio(label="CNPJ", value="cnpj"), # Opção cnpj, altera o value da variável "cpf_cnpj" para "cnpj"
+            ]
+        ),
+
+        value=None,
+        on_change=selecionar_cpf_cnpj, # a cada mudança, chama a função de seleção.
+    )
+
+    # Campo para que o cpf seja digitado:
+    campo_cpf = ft.TextField(label="CPF:", hint_text="EX: XXX.XXX.XXX-XX", visible=False, bgcolor=ft.Colors.WHITE)
+
+    #
 
     def adicionar_cliente(e):
         novo_cliente = {
@@ -30,9 +58,10 @@ def cadastrar_clientes(page, clientes):
             [
                 ft.Row([nome], alignment=ft.MainAxisAlignment.CENTER),
                 ft.Row([numero], alignment=ft.MainAxisAlignment.CENTER),
-                ft.Row([botao_adicionar, botao_cancelar], alignment=ft.MainAxisAlignment.CENTER)
+                ft.Row([cpf_cnpj], alignment=ft.MainAxisAlignment.CENTER),
+                ft.Row([botao_adicionar, botao_cancelar], alignment=ft.MainAxisAlignment.CENTER),
             ],
-            alignment=ft.MainAxisAlignment.CENTER
+            alignment=ft.MainAxisAlignment.CENTER,
         ), 
 
         expand=True,
